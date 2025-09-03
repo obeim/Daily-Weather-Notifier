656a201335812973ae7f31072f39d9e838d8fab0
Obai Mustafa <56155793+obeim@users.noreply.github.com>
2025-09-03 15:14:19 +0300

    feat: add caching to weather forcast endpoint

--- src/routes/weather.ts
+++ src/routes/weather.ts
@@ -3,12 +3,23 @@ import { getWeatherQueryParams } from "../schemas/params.js";
 import { getWeatherForecast } from "../helpers/api.js";
 
 const weatherRoutes: FastifyPluginCallbackTypebox = (fastify, opts) => {
+  const { redis } = fastify;
   fastify.get(
     "/forecast",
     { schema: { querystring: getWeatherQueryParams } },
     async (request, reply) => {
       const { lat, long } = request.query;
+      const cachedData = await redis.get(`weather:${lat},${long}`);
+      if (cachedData) {
+        return JSON.parse(cachedData);
+      }
       const data = await getWeatherForecast(lat, long);
+      await redis.set(
+        `weather:${lat},${long}`,
+        JSON.stringify(data),
+        "EX",
+        7200
+      ); // cache for 2 hours
       return data;
     }
   );
